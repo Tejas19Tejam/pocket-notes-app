285f78d34d914c57c4ed3bedb0abbe36ad39d51d
Vasudev Tejam <88923558+Tejas19Tejam@users.noreply.github.com>
2024-01-23 17:59:47 +0000

    App.jsx modified

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import styles from './app.css'
 
 function App() {
   return <>
-  <aside className={styles.aside}></aside>
+  <aside className={styles.aside}>
+    <h1>Pocket notes</h1>
+    <ul>
+      <li>
+        <p>PY</p>
+        <p>Python Notes</p>
+      </li>
+    </ul>
+  </aside>
   <main  className={styles.main}></main>
   </>
 } 
